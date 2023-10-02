8f958c35a108e93da9688132ad1f7f354931d483
Gulbeyaz <glbzbayram@gmail.com>
2023-10-02 13:34:48 +0300

    data is pulled from contextApi
    datas is pulled from contextApi

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { ScProduct } from "./scParts";
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Product = (props) => {
+  const { addItem, count } = useContext(ProductContext);
+
   return (
     <ScProduct>
       <img src={props.product.image} alt={`${props.product.title} book`} />
@@ -9,8 +13,8 @@ const Product = (props) => {
         <h1 className="title">{props.product.title}</h1>
         <div className="footer">
           <p className="price">${props.product.price}</p>
-          <button onClick={() => props.addItem(props.product)}>
-            Add to cart
+          <button onClick={() => addItem(props.product)}>
+            Add to cart {count}
           </button>
         </div>
       </div>
